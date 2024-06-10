b2534e63633c91d98b3f92e3edddff890b8eae6f
Jason <160682029+Namtabb@users.noreply.github.com>
2024-06-10 11:45:46 -0700

    Made body segment emerald

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ class BodySegment(pg.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
         self.image = pg.Surface((25, 25))
-        self.image.fill(GREEN)
+        self.image.fill(EMERALD)
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
